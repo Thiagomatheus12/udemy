ca2f4727457718d77b01c4c4df5d59592e141487
Thiagomatheus12 <thi_ago5@hotmail.com>
2022-06-03 10:52:06 -0300

    exercicios curso udemy

---
+++ exercicios-js/fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const cliete = function() {}
+console.log(typeof cliete)
+console.log(typeof new cliete)
+
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
---
+++ exercicios-js/fundamentos/unarios.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // num1 = num1 + 1
+console.log(num1)
+--num1 // num1 = num1 - 1
+console.log(num1)
+
+console.log(++num1 === num2--) // num1 = num1 + 1 e num2 = num2 - 1
+console.log(num1 === num2) 
